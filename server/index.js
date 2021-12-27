4ae0cf4851ad2e29b0086409a0d313288e15dcea
Steven Fagen <steven.fagen@bnc.ca>
2021-12-27 09:44:21 -0500

    Add express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express(); // create express app
+
+// When you use the root route, return the index.html file from public folder.
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+// start express server on port 5000
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
