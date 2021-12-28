41906bb89fc0834ba78269e851b0332c8415a1a9
Steven Fagen <steven.fagen@bnc.ca>
2021-12-28 09:55:08 -0500

    Fix counter. Replace setTimeout with setInterval

--- src/hooks/counter.ts
+++ src/hooks/counter.ts
@@ -4,21 +4,29 @@ const ONE_SECOND = 1
 const ONE_SECOND_MS = 1000
 const DEFAULT_INITIAL_COUNTDOWN = 180
 
+let id : ReturnType<typeof setInterval> = undefined
+
 export const useCounter = (initialCount: number = DEFAULT_INITIAL_COUNTDOWN) => {
     const [count, setCount] = useState(initialCount)
+    const [active, setActive] = useState(false)
+
     useEffect(() => {
-      if (count !== 0) {
-        const id = countdown(count)
-        return () => {
-          clearTimeout(id)
-        }
+      if (count === 0) {
+        clearInterval(id)
+        setActive(false)
       }
-      return undefined
     }, [count])
+
+    useEffect(() => {
+      if (count !== 0 && active === false) {
+        setActive(true)
+        id = newCountdown()
+      }
+    }, [count, active])
   
-    const countdown = (c: number) => {
-      return setTimeout(() => {
-        setCount(c - ONE_SECOND)
+    const newCountdown = () => {
+      return setInterval(() => {
+        setCount((c) => c - ONE_SECOND)
       }, ONE_SECOND_MS)
     }
   
